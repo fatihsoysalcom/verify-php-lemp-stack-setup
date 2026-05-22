078ff1f158ac7d8142b9a5115c7c84d685191ce3
Fatih Soysal <fatihsoysal@outlook.com>
2026-05-22 08:01:18 +0300

    feat: initial example code

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// This script demonstrates that PHP is correctly installed and configured
+// as part of your LEMP stack, which is essential for running WordPress.
+
+// The phpinfo() function outputs a large amount of information about the current
+// state of PHP. This includes PHP compilation options and extensions,
+// the server environment, PHP version, host, build date, web servers,
+// and much more. This is a common first step to verify a PHP environment.
+phpinfo();
+?>
